a15d40470b700f6e46e0b7aa5f21236ac3733d11
Oleg Kachanov <ovkachanov1988@gmail.com>
2015-11-11 20:46:00 +0300

    fix spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,4 +4,5 @@ describe Answer do
   it { should validate_presence_of :body }
   it { should validate_presence_of :question_id }
   it { should belong_to :question }
+  it { should have_db_index(:question_id) }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Question do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  it { should have_many :answers }
+  it { should have_many(:answers).dependent(:destroy) }
 end
