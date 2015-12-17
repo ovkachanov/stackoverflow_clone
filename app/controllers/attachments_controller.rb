b312619890a7a3c41781a1b6dca4778137087a86
Oleg Kachanov <ovkachanov1988@gmail.com>
2015-12-17 23:51:30 +0300

    fix remark

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -4,7 +4,7 @@ before_action :authenticate_user!
 
   def destroy
     @attachment = Attachment.find(params[:id])
-    if @attachment.attachable.user_id == current_user.id
+    if current_user.author_of?(@attachment.attachable)
       @attachment.destroy
       flash[:notice] = 'Your attachment deleted'
     end
