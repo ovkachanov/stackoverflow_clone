453ed758dcbf39838df90e3268a19a01945f7447
Oleg Kachanov <ovkachanov1988@gmail.com>
2016-01-10 12:03:39 +0300

    add comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_length_of(:comment_body).is_at_least(5).is_at_most(1000) }
+  it { should belong_to :commentable }
+  it { should belong_to :user}
 end
