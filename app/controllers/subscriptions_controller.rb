9e7660cfd505e9ace68f5a7a694c05758febe2b8
Oleg Kachanov <ovkachanov1988@gmail.com>
2016-02-21 17:01:19 +0300

    fix2

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,5 +1,6 @@
 class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_subscription, only: [:destroy]
   authorize_resource
 
   respond_to :js
@@ -10,7 +11,13 @@ class SubscriptionsController < ApplicationController
   end
 
   def destroy
-    @subscription = current_user.subscriptions.find(params[:id])
     respond_with @subscription.destroy
   end
+
+  private
+
+  def set_subscription
+    @subscription = current_user.subscriptions.find(params[:id])
+  end
 end
+
