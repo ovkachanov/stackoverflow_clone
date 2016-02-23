cbef6d89dc9d0e92f83f34bdfd2354363ee8d293
Oleg Kachanov <ovkachanov1988@gmail.com>
2016-02-23 17:04:20 +0300

    corrected comments

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,14 +3,12 @@ class Search
   CONDITIONS = %w(Questions Answers Comments Users Anything)
 
   def self.query(query, condition)
-    if CONDITIONS.include?(condition)
-      if condition == 'Anything'
-        ThinkingSphinx.search query
-      else
-        ThinkingSphinx.search query, classes: [condition.singularize.classify.constantize]
-      end
+    return [] unless CONDITIONS.include?(condition)
+    escaped_query = Riddle::Query.escape(query)
+    if condition == 'Anything'
+      ThinkingSphinx.search escaped_query
     else
-      []
+      ThinkingSphinx.search escaped_query, classes: [condition.singularize.classify.constantize]
     end
   end
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Search do
+  describe '.search' do
+    %w(Questions Answers Comments Users).each do |obj|
+      it "Array of #{obj}" do
+        expect(ThinkingSphinx).to receive(:search).with('Request', classes: [obj.singularize.classify.constantize])
+        Search.query('Request', "#{obj}")
+      end
+    end
+
+    it "Anything object" do
+      expect(ThinkingSphinx).to receive(:search).with('Request')
+      Search.query('Request', 'Anything')
+    end
+
+    it "Invalid condition" do
+      result = Search.query('Request', 'Nonexistent')
+      expect(result).to eql []
+    end
+  end
 end
