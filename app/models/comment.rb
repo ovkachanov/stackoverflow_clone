d70e23b0975a351b93076bcd31655996b8d14a32
Oleg Kachanov <ovkachanov1988@gmail.com>
2016-03-08 20:34:48 +0300

    add cache

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
 
   belongs_to :user
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
 
   validates :comment_body, length: { in: 5..1000 }
 
